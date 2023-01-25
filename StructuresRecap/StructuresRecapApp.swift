0270ae65a37ab672fff9a67519fc98dd03703f45
Ruiyan Liang <Jliang@lcs.on.ca>
2023-01-25 11:18:38 -0500

    Initial Commit

---
+++ StructuresRecap/StructuresRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructuresRecapApp.swift
+//  StructuresRecap
+//
+//  Created by dyhidrogen monoxide on 2023/1/25.
+//
+
+import SwiftUI
+
+@main
+struct StructuresRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
